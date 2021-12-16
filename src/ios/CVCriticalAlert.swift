fb03fb5ebc87fcc542b2605ea13c4cabad97146f
hiroki <hiroki@arcadia.co.jp>
2021-12-16 11:33:16 +0900

    Add has permission method

--- src/ios/CVCriticalAlert.swift
+++ src/ios/CVCriticalAlert.swift
@@ -19,3 +19,29 @@ import Foundation
         }
     }
 }
+
+@objc(CVCriticalAlert) class CVCriticalAlert: CDVPlugin {
+    @objc(hasPermission:)
+    func hasPermission(command: CDVInvokedUrlCommand) {
+        // get permission
+        var authOptions: UNAuthorizationOptions?
+        if #available(iOS 12.0, *) {
+            authOptions = [.alert, .badge, .sound, .criticalAlert]
+        } else {
+            authOptions = [.alert, .badge, .sound]
+        }
+        let semaphore = DispatchSemaphore(value: 0)
+        UNUserNotificationCenter.current().getNotificationSettings(completionHandler: {setting in
+            if setting.authorizationStatus == .authorized {
+                print("許可")
+            } else {
+                print("未許可")
+            }
+            print(setting)
+            semaphore.signal()
+            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAsDictionary: setting)
+            self.commandDelegate!.send(result, callbackId: command.callbackId)
+        })
+        semaphore.wait()
+    }
+}
